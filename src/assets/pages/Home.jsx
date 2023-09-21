a5eed02de60e0e46754ca4bd13c341c45646e119
davideee28 <145684304+davideee28@users.noreply.github.com>
2023-09-21 16:23:51 +0200

    Update Home.jsx

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react";
 import styles from "./Home.module.css";
 import fetcher from "../../fetcher";
 import Card from "../components/Card";
-import Slider from "../components/slider";
+import Slider from "../components/Slider";
 
 export default function Home() {
   const [upcomingMovie, setUpcomingMovie] = useState();
   const [topRatedSeries, setTopRatedSeries] = useState();
 
   const getData = async () => {
-    const newUpcoming = await fetcher.default("movie/upcoming").then(
-      (response) => response
-    );
-    const newTopRated = await fetcher.default("tv/top_rated").then(
-      (response) => response
-    );
+    const newUpcoming = await fetcher
+      .default("movie/upcoming")
+      .then((response) => response);
+    const newTopRated = await fetcher
+      .default("tv/top_rated")
+      .then((response) => response);
     setUpcomingMovie(newUpcoming.results);
     setTopRatedSeries(newTopRated.results);
   };
@@ -24,9 +24,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className="titoli"> UPCOMING MOVIES </div>
-      <Slider data={upcomingMovie} type="movie" title="title"/>
+      <Slider data={upcomingMovie} type="movie" title="title" />
       <div className="titoli"> TOP RATED SERIES </div>
-      <Slider data={topRatedSeries} type="tv" title="name"/>
+      <Slider data={topRatedSeries} type="tv" title="name" />
     </div>
   );
 }
